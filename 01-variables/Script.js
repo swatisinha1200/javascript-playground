2d270444b837765204bcea5e526282f8d0da5330
swati sinha <swatisinha@gmail.com>
2026-07-22 14:20:02 +0530

    feat: problem soved

---
+++ 01-variables/Script.js
@@ -0,0 +1,65 @@
+// ======================================
+// Exercise 1:Create variables to store:
+// 	• Your full name
+// 	• Your age
+// 	• Your favorite programming language
+// Print all three values using console.log().
+
+//=======================================
+
+let name = "Swati sinha";
+let age = 30;
+let programming = "Javascript";
+
+console.log(
+  ` my name is : ${name} and age ${age}. My favorite programming language is ${programming} `,
+);
+
+//==============================================================================
+// Exercise 2 : Create a variable named balance and assign it an initial value.
+// Increase the balance by 200.
+// Print the updated balance.
+// ==============================================================================
+
+let balance = 50;
+balance += 200;
+console.log(` updated balance ${balance}`);
+
+//==============================================================================
+// Exercise 3 : Create the following variables:
+// 	• isStudent
+// 	• isEmployed
+// Assign appropriate boolean values and print them.
+
+// ==============================================================================
+
+const isStudent = true;
+const isEmployed = false;
+console.log(isStudent);
+console.log(isEmployed);
+
+//================================================================
+// Exercise 4 : Create a variable called city.
+// Print the sentence:
+// I live in <city>.
+//================================================================
+
+const city = "Hyderabad";
+console.log(`I live in ${city}.`);
+
+//================================================================
+// Exercise 5: Create variables for:
+// 	• firstName
+// 	• lastName
+// 	• age
+// 	• city
+// Print a short self-introduction using those variables.
+
+//================================================================
+const firstName = "Swati";
+const lastName = "sinha";
+const age = 30;
+const city = "Hyderabad";
+console.log(
+  `Hi my name is ${firstName} ${lastName} and age is ${age}. I live in city ${city}`,
+);
---
+++ 02-data-types/Script.js
@@ -0,0 +1,72 @@
+//===================================================
+//Exercise 1 :Create one variable for each data type:
+// 	• String
+// 	• Number
+// 	• Boolean
+// 	• Undefined
+// 	• Null
+// Print each variable.
+// ===================================================
+
+const name = "Ram";
+const age = 12;
+const isStudent = true;
+let city;
+const state = null;
+console.log(
+  `my name is ${name} and age is ${age} and i am a student ${isStudent} . city is ${city} and state is ${state} `,
+);
+//========================================================
+// Exercise 2
+// Use the typeof operator to print the type of:
+// 	• A string
+// 	• A number
+// 	• A boolean
+//=======================================================
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+
+// ==========================================================
+
+// Exercise 3
+// Create a variable called temperature.
+// Assign it a number.
+// Print:
+// 	• the value
+// 	• its type
+
+//========================================================
+
+const temperature = 67;
+console.log(
+  `Temperature value is ${temperature}and it's type is ${typeof temperature}`,
+);
+
+//===========================================================
+// Create variables for:
+// 	• name
+// 	• age
+// 	• isEmployed
+// 	• salary
+// 	• city
+// Print both:
+// 	1. The value
+// 	2. Its data type
+
+// ========================================================
+const name = "Abdul";
+const age = 38;
+const isEmployed = false;
+const salary = null;
+let city;
+
+console.log(
+  `my name is ${name} and age is ${age}. Employment Status ${isEmployed}  and salary is ${salary}. i live in city ${city}`,
+);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isEmployed);
+console.log(typeof salary);
+console.log(typeof city);
